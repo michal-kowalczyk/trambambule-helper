8b6f00fb2ecd9b1a0d8cb89aecdc89f792ad9280
mkk <mkk@ekk.pl>
2017-04-25 00:47:34 +0200

    C++ refactoring, build.gradle upgrade to sdk 24

--- src/cpp/GameImpl.cpp
+++ src/cpp/GameImpl.cpp
@@ -6,12 +6,15 @@
 #include <memory>
 #include <string>
 
-namespace djinnidemo {
+namespace djinnidemo
+{
 
-class GameImpl : public Game {
- public:
-  GameImpl(const PointsCanvasMetrics& ui, int32_t totalPoints,
-      const std::shared_ptr<PointListener>& l)
+class GameImpl : public Game
+{
+public:
+  GameImpl(const PointsCanvasMetrics& ui,
+           const int32_t totalPoints,
+           const std::shared_ptr<PointListener>& l)
     : totalPoints_(totalPoints),
       listener_(l),
       pointSize_((ui.bottom - ui.top) / (totalPoints + 1) - ui.spacing),
@@ -27,25 +30,35 @@ class GameImpl : public Game {
     std::string test = std::to_string(0);
   }
 
-  void gainPoint(Team t) {
-    if (Team::RED == t) {
+  void gainPoint(Team t) override
+  {
+    if (Team::RED == t)
+    {
       gainPt(t, redPoints_, redPt0X_, redPt0Y_, redShift_);
-    } else {
+    }
+    else
+    {
       gainPt(t, bluePoints_, bluePt0X_, bluePt0Y_, blueShift_);
     }
   }
 
-  void losePoint(Team t) {
-    if (Team::RED == t) {
+  void losePoint(Team t) override
+  {
+    if (Team::RED == t)
+    {
       losePt(t, redPoints_, redPt0X_, redPt0Y_, redShift_);
-    } else {
+    }
+    else
+    {
       losePt(t, bluePoints_, bluePt0X_, bluePt0Y_, blueShift_);
     }
   }
 
  private:
-  void createPoints(int32_t pointSize) {
-    for (int32_t i = 0; i < totalPoints_; ++i) {
+  void createPoints(const int32_t pointSize)
+  {
+    for (int32_t i = 0; i < totalPoints_; ++i)
+    {
       PointCreationEvent e1(Team::RED,
           i, redPt0X_, i * redShift_ + redPt0Y_, pointSize);
       listener_->onCreation(e1);
@@ -55,23 +68,35 @@ class GameImpl : public Game {
     }
   }
 
-  void gainPt(Team t, int32_t& points, int32_t pt0X, int32_t pt0Y, int32_t shift) {
-    if (points >= totalPoints_) {
+  void gainPt(const Team t,
+              int32_t& points,
+              const int32_t pt0X,
+              const int32_t pt0Y,
+              const int32_t shift)
+  {
+    if (points >= totalPoints_)
+    {
       return;
     }
     ++points;
-    int32_t pointId = totalPoints_ - points;
-    int32_t pos = pointId + 1;
-    PointRepositionEvent e(t, pointId, pt0X, pos * shift + pt0Y);
+    const int32_t pointId = totalPoints_ - points;
+    const int32_t pos = pointId + 1;
+    const PointRepositionEvent e(t, pointId, pt0X, pos * shift + pt0Y);
     listener_->onReposition(e);
   }
 
-  void losePt(Team t, int32_t& points, int32_t pt0X, int32_t pt0Y, int32_t shift) {
-    if (points <= 0) {
+  void losePt(const Team t,
+              int32_t& points,
+              const int32_t pt0X,
+              const int32_t pt0Y,
+              const int32_t shift)
+  {
+    if (points <= 0)
+    {
       return;
     }
-    int32_t pointId = totalPoints_ - points;
-    int32_t pos = pointId;
+    const int32_t pointId = totalPoints_ - points;
+    const int32_t pos = pointId;
     --points;
     PointRepositionEvent e(t, pointId, pt0X, pos * shift + pt0Y);
     listener_->onReposition(e);
@@ -79,7 +104,7 @@ class GameImpl : public Game {
 
   const int32_t totalPoints_;
 
-  std::shared_ptr<PointListener> listener_;
+  const std::shared_ptr<PointListener> listener_;
 
   const int32_t pointSize_;
   const int32_t redPt0X_;
@@ -94,7 +119,9 @@ class GameImpl : public Game {
 };
 
 std::shared_ptr<Game> Game::create(const PointsCanvasMetrics& metrics,
-    int32_t totalPoints, const std::shared_ptr<PointListener>& listener) {
+                                   int32_t totalPoints,
+                                   const std::shared_ptr<PointListener>& listener)
+{
   return std::make_shared<GameImpl>(metrics, totalPoints, listener);
 }
 
